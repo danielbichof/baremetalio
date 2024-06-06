26c334d830f5e79d6d4f85499ddbc74f67a6ca49
danielbichof <daniel.h.bichof@gmail.com>
2024-06-06 19:41:41 -0300

    Criando branch para usar

--- src/test/java/com/bichof/baremetalio/BareMetalIOApplication.java
+++ src/test/java/com/bichof/baremetalio/BareMetalIOApplication.java
@@ -1,13 +1,17 @@
 package com.bichof.baremetalio;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+
 class BareMetalIOApplication {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 1);
 		return;
 	}
 
